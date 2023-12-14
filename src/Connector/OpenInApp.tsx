e4ef4dcb77765bd83d3e74f1bd3484fc7002e2dd
AZbang <azbangwtf@ya.ru>
2023-12-14 12:17:27 +0100

    add delete page

--- src/Connector/OpenInApp.tsx
+++ src/Connector/OpenInApp.tsx
@@ -10,6 +10,47 @@ const OpenInApp = () => {
     .replace(window.location.origin, "herewallet:/")
     .replace(window.location.origin, "herewallet:/");
 
+  if (window.location.pathname === "/command/delete") {
+    return (
+      <S.Page>
+        <S.Wrap style={{ padding: 32 }}>
+          <H1 style={{ textAlign: "center", lineHeight: 0.9, marginBottom: 16 }}>
+            Delete HERE account?
+          </H1>
+
+          {isMobile() ? (
+            <>
+              <Text style={{ fontSize: 20, textAlign: "center" }}>
+                To perform this action, you have to go to the application.
+              </Text>
+              <ActionButton as="a" href={link} style={{ borderRadius: 16, marginTop: 32 }}>
+                Tap to open in HERE
+              </ActionButton>
+            </>
+          ) : (
+            <>
+              <Text style={{ fontSize: 20, marginBottom: 32, textAlign: "center" }}>
+                To perform this action, you have to scan this QR code
+              </Text>
+              <HereQRCode value={link} />
+            </>
+          )}
+
+          <div style={{ maxWidth: 500, marginTop: 16, textAlign: "center" }}>
+            <Text style={{}}>
+              1. HERE account will be deleted - score, achievements, friends, contacts and other personal
+              data will be deleted immediately
+            </Text>
+            <Text style={{ marginTop: 8 }}>
+              2. You will retain access to your tokens and NFTs via a seed phrase
+            </Text>
+          </div>
+        </S.Wrap>
+        <Footer />
+      </S.Page>
+    );
+  }
+
   return (
     <S.Page>
       <S.Wrap style={{ padding: 32 }}>
--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import pluginRewriteAll from "vite-plugin-rewrite-all";
 import viteTsconfigPaths from "vite-tsconfig-paths";
-import { viteSingleFile } from "vite-plugin-singlefile";
 import svgrPlugin from "vite-plugin-svgr";
 import { resolve } from "path";
-
-const isExport = process.env.BUILD_ID === "export";
-const input = {};
-
-if (isExport) {
-  input.export = resolve(__dirname, "export/index.html");
-} else {
-  input.connector = resolve(__dirname, "connector/index.html");
-  input.index = resolve(__dirname, "index.html");
-}
 
 export default defineConfig({
   define: {
@@ -29,7 +18,8 @@ export default defineConfig({
       include: [],
     },
     rollupOptions: {
-      input,
+      connector: resolve(__dirname, "connector/index.html"),
+      index: resolve(__dirname, "index.html"),
     },
   },
   resolve: {
@@ -47,10 +37,5 @@ export default defineConfig({
       util: "util",
     },
   },
-  plugins: [
-    pluginRewriteAll(),
-    react(),
-    viteTsconfigPaths(),
-    svgrPlugin({ exportAsDefault: true }),
-  ].concat(isExport ? [viteSingleFile()] : []),
+  plugins: [pluginRewriteAll(), react(), viteTsconfigPaths(), svgrPlugin({ exportAsDefault: true })],
 });
