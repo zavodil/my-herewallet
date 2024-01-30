2d23a09199a56a29b8fa5816188b2150d0474181
AZbang <azbangwtf@ya.ru>
2024-01-31 02:10:08 +0300

    fix

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -117,8 +117,8 @@ const Home = () => {
             )}
           </Card>
 
-          {isTgMobile() && !account.hot.needRegister && <Widgets />}
-          {isTgMobile() && account.hot.needRegister && (
+          {isTgMobile() && account.hot.balance === 0 && <Widgets />}
+          {isTgMobile() && account.hot.balance > 0 && (
             <div
               style={{
                 borderRadius: 20,
